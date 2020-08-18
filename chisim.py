c2592c7099b2ee62c55440c8c96b6b89230d5502
Guglielmo Pellitteri <67461869+lateral-gug@users.noreply.github.com>
2020-08-18 12:41:52 +0200

    Update chisim.py

--- chisim.py
+++ chisim.py
@@ -1,29 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-gdl = 50
-campione = 10**7
-colonne = 1000
+gdl = 50  #degrees of freedom
+campione = 10**7  #generated points
+colonne = 1000  #number columns of the plotted histogram
 
 x = np.random.chisquare(gdl,campione)
-plt.hist(x, colonne, color='red', density=True,histtype='step',label='Chi quadro a %.0f g.d.l.'%(gdl))
+plt.hist(x, colonne, color='red', density=True, histtype='step', label='$\chi^2$ a %.0f g.d.l.'%(gdl))
 
 y = np.random.normal(gdl,np.sqrt(2*gdl),campione)
-plt.hist(y, colonne, color='black', density=True, histtype='step',label='Gaussiana associata')
+plt.hist(y, colonne, color='black', density=True, histtype='step', label='Gaussiana associata')
 
 plt.xlabel('$x$')
 plt.ylabel('$p(x)$')
 plt.grid(linestyle=':')
-plt.legend(loc='upper right',shadow=True)
-
-#plt.figure(2)
-chirid = (x-gdl)/np.sqrt(2*gdl)
-#plt.hist(chirid,colonne,color='blue',density=True,histtype='step',label='Chi ridotto a %.0f g.d.l.'%(gdl))
-
-plt.xlabel('$x$')
-plt.ylabel('$p(x)$')
-plt.grid(linestyle=':')
-plt.legend(loc='upper right',shadow=True)
+plt.legend(loc='upper right', shadow=True)
 
 print('\n Chi quadro: %.3f +- %.3f'%(np.mean(x),np.std(x)))
 print('\n Variabile Gaussiana: %.3f +- %.3f'%(np.mean(y),np.std(y)))
